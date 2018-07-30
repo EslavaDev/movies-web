5566c311f35c74bedae7f877c161416e99b63897
WOLFDEV\Eslava <danieleslava52@gmail.com>
2018-07-30 08:44:17 -0500

    config

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById('app');
+
+//ReactDOM.render(que voy a renderizar, donde lo voy ahcer)
+const holaMundo = <h1>hola mundo!!!</h1>;
+ReactDOM.render(holaMundo,app);
